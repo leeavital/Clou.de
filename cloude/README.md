6cf1fbe568cc45ca8de944f1658db6e7f37ee37b
Lee Avital <leeavital@gmail.com>
2014-03-26 16:00:54 -0400

    Add scala site

---
+++ cloude/README.md
@@ -0,0 +1,13 @@
+# ClouDe #
+
+## Build & Run ##
+
+```sh
+$ cd ClouDe
+$ ./sbt
+> container:start
+> browse
+> ~ ;copy-resources;aux-compil
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ cloude/src/main/scala/edu/lja5234/cloude/CloudeServlet.scala
@@ -0,0 +1,87 @@
+package edu.lja5234.cloude
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+
+// JSON-related libraries
+import org.json4s.{DefaultFormats, Formats}
+
+// JSON handling support from Scalatra
+import org.scalatra.json._
+
+import scala.collection.JavaConverters._
+
+import scala.util.{Try, Success, Failure}
+
+
+
+
+case class SourceFile( name : String, source : String );
+
+
+
+object DevEnvironment{
+
+  def saveFile ( f : SourceFile ) = {
+    val SourceFile(name, source) = f
+
+    val ofile = new java.io.File( "workspace/" + name )
+    val ps = new java.io.PrintWriter( ofile )
+    ps.print( source )
+
+    ps.close
+  }
+
+
+
+}
+
+
+
+class CloudeServlet extends CloudeStack with JacksonJsonSupport{
+
+  protected implicit val jsonFormats : Formats = DefaultFormats
+  var parsedRequestBody : org.json4s.JValue = null
+
+  before(){
+    contentType = formats( "json" )
+    parsedRequestBody = Try[org.json4s.JValue]( parse( org.json4s.StringInput( request.body ) ) ) match {
+      case Success(v) => v
+      case Failure(e) => null
+
+    }
+  }
+
+  // save a file -- takes a SourceF.ile
+  post( "/file" ) {
+    val file = parsedRequestBody.extract[SourceFile]
+    DevEnvironment saveFile( file )
+    println ( request.body )
+    println( file )
+    "ok"
+  }
+
+  // get the contents of a file -- returns a source file
+  get( "/file/:fname" ) {
+    val source = scala.io.Source.fromFile( "workspace/" + params("fname") )
+    val lines = source.mkString
+    source.close()
+
+    SourceFile( params("fname"), lines )
+  }
+
+
+  delete( "/file/:fname" ){
+    val file = new java.io.File( "workspace/" + params( "fname" ) )
+    file.delete
+  }
+
+
+  // get a list of files
+  get( "/files" ){
+    val ws = new java.io.File( "workspace" )
+    (ws.list) toList
+  }
+
+}
