43505e997c62749b1f6b02fb0dc6e181bbc529ca
Lee Avital <leeavital@gmail.com>
2014-04-13 15:09:57 -0400

    Put all js into app.js

---
+++ cloude/src/main/webapp/app.js
@@ -0,0 +1,35 @@
+var ClouDe = angular.module( 'ClouDe', [] );
+console.log( ClouDe );
+
+var Ctl = ClouDe.controller( 'Ctl', [ '$scope', '$http', function( $scope, $http ){
+
+    $scope.getSourceFiles = function( cb ){
+    $http.get( '/files' ).then( function(response) {
+      $scope.sourceFiles = response.data;
+
+      cb( $scope.sourceFiles);
+      });
+    }
+
+
+    $scope.openFile = function( file ){
+    $http.get( '/file/' + file ).then( function(response){
+      $scope.sourceFile = response.data;
+      });
+    }
+
+
+    $scope.save = function(){
+    $http.post( '/file', $scope.sourceFile );
+    }
+
+    $scope.getSourceFiles( function( files ){
+        $scope.openFile( files[0] ); 
+        });
+
+}]);
+
+
+console.log( Ctl );
+
+
