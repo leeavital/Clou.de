e73efa2a3636ac34637151979dc5f701af8dc35d
Lee Avital <leeavital@gmail.com>
2014-05-18 22:48:29 -0400

    Fix DevEnvironment bugs

--- cloude/src/main/scala/edu/lja5234/cloude/CloudeServlet.scala
+++ cloude/src/main/scala/edu/lja5234/cloude/CloudeServlet.scala
@@ -12,16 +12,14 @@ import org.json4s.{DefaultFormats, Formats}
 // JSON handling support from Scalatra
 import org.scalatra.json._
 
+// JDK
 import scala.collection.JavaConverters._
+import java.io.{File => JFile}
 
 import scala.util.{Try, Success, Failure}
 
-
-
-
-
 /**
- * name will have the .java suffix
+ * name will have the .java suffix and be relative to workspace
  */
 case class SourceFile( name : String, source : String );
 
@@ -43,15 +41,41 @@ object DevEnvironment{
   def saveFile ( f : SourceFile ) = {
     val SourceFile(name, source) = f
 
-    val ofile = new java.io.File( "workspace/" + name )
+    val localuri = "workspace/" + name
+    val pattern = """(.*/)([^\.]*\.java)""".r
+    val pattern( pre, filename ) = localuri
+
+    println( pre )
+    println( filename )
+    println( f )
+
+    // create the directories
+    (new JFile( pre )).mkdirs()
+
+    val ofile = new JFile( pre + filename )
+    println( ofile )
+
     val ps = new java.io.PrintWriter( ofile )
 
+    ps.println( f.source )
     ps.close
+    println( "wrote to " + f.name )
+  }
+
+  def deleteFile( file :String ) = {
+    (new JFile("./workspace/" + file)).delete
+
   }
 
 
 
   def compileFile( fname : String ) = {
+    // delete the original class file
+    val clspath = ("workspace_dist/" + fname).replace( ".java", ".class" )
+    val f = new java.io.File( clspath )
+    f.delete()
+
+
     val filepath = "workspace/" + fname
     val compiler = ToolProvider.getSystemJavaCompiler();
     var errStream = new ErrorStream
@@ -61,13 +85,36 @@ object DevEnvironment{
 
 
   def compileAll( ) = {
-    val compiler= ToolProvider.getSystemJavaCompiler()
-    compiler.run( System.in, System.out, System.err, "workspace/*.java", "-d",  "workspace_dist" )
+
+
+    val javaFiles = DevEnvironment.files.map( x => new JFile( "workspace/" + x ) )
+    println( javaFiles )
+
+    val compiler = ToolProvider.getSystemJavaCompiler();
+    //note that the "compiler" may be null if your environment path is not set correctly
+
+
+    val dc = new DiagnosticCollector[JavaFileObject];
+
+    val sjfm = compiler.getStandardFileManager(dc, null, null);
+
+    val fileObjects = sjfm.getJavaFileObjectsFromFiles(javaFiles.asJava); //javaFiles is an array of File
+
+    val options = List("-d", "workspace_dist" ).asJava
+
+    val errorstream =  new ErrorStream()
+    val ewriter = new java.io.PrintWriter( errorstream )
+    compiler.getTask(null, sjfm, dc, options, null, fileObjects).call();
+
+    for( d <- dc.getDiagnostics.asScala ) {
+      errorstream.write( d.toString.getBytes )
+    }
+
+    errorstream.errors
   }
 
 
-  def run (r : RunConfig ) = {
-
+  def run (r : RunConfig ) : String = {
     // swap out error streams -- this is really gross, I should probably subclass PrintStream
     val sysout = System.out
     val syserr = System.err
@@ -80,14 +127,38 @@ object DevEnvironment{
     val classpath = Array( new java.io.File( "workspace_dist" ).toURL )
     val classloader = new java.net.URLClassLoader( classpath );
 
+
     // load the main class and run the main method with the given args
     val main = classloader.loadClass( r.main );
     main.newInstance.asInstanceOf[{def main(args : Array[String] ) : Unit }].main( r.args.toArray );
 
+
     System.setOut( sysout )
     System.setErr( syserr )
 
+
     ss.getContents
+  }
+
+
+  // get a list of all the files in this dev environment (relative to the workspace)
+  def files : List[String] = {
+
+    val root : JFile = new JFile( "./workspace" )
+    val prefix = root.getCanonicalPath + "/"
+    def helper( f : JFile ) : List[String] = {
+        f.list match {
+          case null =>
+            List( f.getCanonicalPath replace(prefix, "")  )
+          case (subs: Array[String]) =>
+            val subfiles = subs.toList.map( new JFile( f.getCanonicalPath, _ ) )
+            val sublistings : List[List[String]]= subfiles.map( helper( _ ) )
+            List.flatten( sublistings )
+        }
+    }
+
+    val e = helper( root )
+    e
   }
 
 
@@ -118,12 +189,14 @@ class CloudeServlet extends CloudeStack with JacksonJsonSupport{
   }
 
   // get the contents of a file -- returns a source file
-  get( "/file/:fname" ) {
-    val source = scala.io.Source.fromFile( "workspace/" + params("fname") )
+  get( "/file/*" ) {
+    val Seq(fname : String ) = multiParams( "splat" )
+
+    val source = scala.io.Source.fromFile( "workspace/" + fname )
     val lines = source.mkString
     source.close()
 
-    SourceFile( params("fname"), lines )
+    SourceFile( fname , lines )
   }
 
 
@@ -133,15 +206,12 @@ class CloudeServlet extends CloudeStack with JacksonJsonSupport{
   }
 
 
-  // get a list of files
+  // get a list of all the source files
+  // as  a list of strings
   get( "/files" ){
-    val ws = new java.io.File( "workspace" )
-    (ws.list) toList
+    (DevEnvironment.files : List[String])
   }
 
-  get( "/compile" ){
-    DevEnvironment.compileAll( )
-  }
 
 
   post( "/run" ) {
@@ -150,9 +220,8 @@ class CloudeServlet extends CloudeStack with JacksonJsonSupport{
   }
 
 
-  post( "/compile" ){
-    val sf  : String = parsedRequestBody.extract[String];
-    val errors = DevEnvironment.compileFile( sf  )
+  get( "/compile" ){
+    val errors = DevEnvironment.compileAll
 
     errors
   }
